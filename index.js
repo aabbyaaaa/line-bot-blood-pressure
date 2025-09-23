d1b58ca410308d5bc2b9d9c1510f9e6045cfe986
aabbyaaaa <aaaaaabby@gmail.com>
2025-09-23 13:44:27 +0800

    最新的 index.js 修改

--- index.js
+++ index.js
@@ -560,63 +560,67 @@ function getFlexMessage() {
 
 function getFlexMessage2() {
   return {
-    type: "carousel",
-    contents: [
-      {
-        type: "bubble",
-        body: {
-          type: "box",
-          layout: "vertical",
-          contents: [
-            {
-              type: "image",
-              url: "https://cdn.shopify.com/s/files/1/0204/3327/2854/files/8_edcd2cad-c171-45ef-9482-44ad5f2b70c2.jpg?v=1758603330",
-              size: "full",
-              aspectMode: "cover",
-              aspectRatio: "1:1",
-              gravity: "center",
-            },
-          ],
-          paddingAll: "0px",
+    type: "flex",
+    altText: "現在優惠", // 加上 altText
+    contents: {
+      type: "carousel",
+      contents: [
+        {
+          type: "bubble",
+          body: {
+            type: "box",
+            layout: "vertical",
+            contents: [
+              {
+                type: "image",
+                url: "https://cdn.shopify.com/s/files/1/0204/3327/2854/files/8_edcd2cad-c171-45ef-9482-44ad5f2b70c2.jpg?v=1758603330",
+                size: "full",
+                aspectMode: "cover",
+                aspectRatio: "1:1",
+                gravity: "center",
+              },
+            ],
+            paddingAll: "0px",
+          },
         },
-      },
-      {
-        type: "bubble",
-        body: {
-          type: "box",
-          layout: "vertical",
-          contents: [
-            {
-              type: "image",
-              url: "https://cdn.shopify.com/s/files/1/0204/3327/2854/files/5_2edb8052-f42f-4306-b8e5-e2eaee5028d6.jpg?v=1758603329",
-              size: "full",
-              aspectMode: "cover",
-              aspectRatio: "1:1",
-              gravity: "center",
-            },
-          ],
-          paddingAll: "0px",
+        {
+          type: "bubble",
+          body: {
+            type: "box",
+            layout: "vertical",
+            contents: [
+              {
+                type: "image",
+                url: "https://cdn.shopify.com/s/files/1/0204/3327/2854/files/5_2edb8052-f42f-4306-b8e5-e2eaee5028d6.jpg?v=1758603329",
+                size: "full",
+                aspectMode: "cover",
+                aspectRatio: "1:1",
+                gravity: "center",
+              },
+            ],
+            paddingAll: "0px",
+          },
         },
-      },
-      {
-        type: "bubble",
-        body: {
-          type: "box",
-          layout: "vertical",
-          contents: [
-            {
-              type: "image",
-              url: "https://cdn.shopify.com/s/files/1/0204/3327/2854/files/20241208.jpg?v=1758603697",
-              size: "full",
-              aspectMode: "cover",
-              aspectRatio: "1:1",
-              gravity: "center",
-            },
-          ],
-          paddingAll: "0px",
+        {
+          type: "bubble",
+          body: {
+            type: "box",
+            layout: "vertical",
+            contents: [
+              {
+                type: "image",
+                url: "https://cdn.shopify.com/s/files/1/0204/3327/2854/files/20241208.jpg?v=1758603697",
+                size: "full",
+                aspectMode: "cover",
+                aspectRatio: "1:1",
+                gravity: "center",
+              },
+            ],
+            paddingAll: "0px",
+          },
         },
-      },
-    ],
+      ],
+    },
   };
 }
 
